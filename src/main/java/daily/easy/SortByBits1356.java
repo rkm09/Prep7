4d2cc78ff53a6d62e922e4cda7a83e506021a67b
rkm09 <raksha.mishra09@gmail.com>
2026-02-25 13:48:02 +0530

    sort by bits

---
+++ src/main/java/daily/easy/SortByBits1356.java
@@ -0,0 +1,44 @@
+package daily.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortByBits1356 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,4,5,6,7,8};
+        System.out.println(Arrays.toString(sortByBits(arr)));
+    }
+
+    public static int[] sortByBits(int[] arr) {
+        List<Integer> li = new ArrayList<>();
+        for(int a : arr) li.add(a);
+        li.sort(Comparator.comparingInt(Integer::bitCount)
+                .thenComparing(a -> a));
+        int[] res = new int[li.size()];
+        for(int i = 0 ; i < res.length ; i++)
+            res[i] = li.get(i);
+        return res;
+    }
+}
+
+/*
+You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+Return the array after sorting it.
+Example 1:
+Input: arr = [0,1,2,3,4,5,6,7,8]
+Output: [0,1,2,4,8,3,5,6,7]
+Explanation: [0] is the only integer with 0 bits.
+[1,2,4,8] all have 1 bit.
+[3,5,6] have 2 bits.
+[7] has 3 bits.
+The sorted array by bits is [0,1,2,4,8,3,5,6,7]
+Example 2:
+Input: arr = [1024,512,256,128,64,32,16,8,4,2,1]
+Output: [1,2,4,8,16,32,64,128,256,512,1024]
+Explanation: All integers have 1 bit in the binary representation, you should just sort them in ascending order.
+Constraints:
+1 <= arr.length <= 500
+0 <= arr[i] <= 104
+ */
