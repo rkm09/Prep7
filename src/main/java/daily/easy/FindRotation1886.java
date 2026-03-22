107910529ddde74a3801d2d024d2604d1f550e54
rkm09 <raksha.mishra09@gmail.com>
2026-03-22 23:00:39 +0530

    find rotation

--- src/main/java/daily/easy/FindRotation1886.java
+++ src/main/java/daily/easy/FindRotation1886.java
@@ -1,6 +1,6 @@
 package daily.easy;
 
-public class FineRotation1886 {
+public class FindRotation1886 {
     public static void main(String[] args) {
         int[][] mat = {{0,1},{1,0}};
         int[][] target = {{1,0},{0,1}};
