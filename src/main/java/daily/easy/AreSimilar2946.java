718466fa429726feaa4dc2f44b8b5cbbaadbc3d8
rkm09 <raksha.mishra09@gmail.com>
2026-03-27 22:42:15 +0530

    similar

---
+++ src/main/java/daily/easy/AreSimilar2946.java
@@ -0,0 +1,47 @@
+package daily.easy;
+
+public class AreSimilar2946 {
+    public static void main(String[] args) {
+        int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(areSimilar(mat, 4));
+    }
+
+//    traversal; time: O(m.n), space: O(1)
+    public static boolean areSimilar(int[][] mat, int k) {
+        int n = mat[0].length;
+        for(int[] row : mat) {
+            for(int j = 0 ; j < n ; j++) {
+                if(row[j] != row[(j + k) % n])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
+
+/*
+You are given an m x n integer matrix mat and an integer k. The matrix rows are 0-indexed.
+The following process happens k times:
+Even-indexed rows (0, 2, 4, ...) are cyclically shifted to the left.
+Odd-indexed rows (1, 3, 5, ...) are cyclically shifted to the right.
+Return true if the final modified matrix after k steps is identical to the original matrix, and false otherwise.
+Example 1:
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]], k = 4
+Output: false
+Explanation:
+In each step left shift is applied to rows 0 and 2 (even indices), and right shift to row 1 (odd index).
+Example 2:
+Input: mat = [[1,2,1,2],[5,5,5,5],[6,3,6,3]], k = 2
+Output: true
+Explanation:
+Example 3:
+Input: mat = [[2,2],[2,2]], k = 3
+Output: true
+Explanation:
+As all the values are equal in the matrix, even after performing cyclic shifts the matrix will remain the same.
+Constraints:
+1 <= mat.length <= 25
+1 <= mat[i].length <= 25
+1 <= mat[i][j] <= 25
+1 <= k <= 50
+ */
