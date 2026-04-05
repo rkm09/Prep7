2f45546ce5accaefaf64eb59e750ace2c64bac35
rkm09 <raksha.mishra09@gmail.com>
2026-04-05 15:48:02 +0530

    judge circle

---
+++ src/main/java/daily/easy/JudgeCircle657.java
@@ -0,0 +1,39 @@
+package daily.easy;
+
+public class JudgeCircle657 {
+    public static void main(String[] args) {
+        System.out.println(judgeCircle("UD"));
+    }
+
+    public static boolean judgeCircle(String moves) {
+        int horizontal = 0, vertical = 0;
+        for(char dir : moves.toCharArray()) {
+            switch (dir) {
+                case 'R' : horizontal++; break;
+                case 'L' : horizontal--; break;
+                case 'U' : vertical++; break;
+                case 'D' : vertical--;
+            }
+        }
+
+        return horizontal == 0 && vertical == 0;
+    }
+}
+
+/*
+There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, judge if this robot ends up at (0, 0) after it completes its moves.
+You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move. Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
+Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.
+Note: The way that the robot is "facing" is irrelevant. 'R' will always make the robot move to the right once, 'L' will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+Example 1:
+Input: moves = "UD"
+Output: true
+Explanation: The robot moves up once, and then down once. All moves have the same magnitude, so it ended up at the origin where it started. Therefore, we return true.
+Example 2:
+Input: moves = "LL"
+Output: false
+Explanation: The robot moves left twice. It ends up two "moves" to the left of the origin. We return false because it is not at the origin at the end of its moves.
+Constraints:
+1 <= moves.length <= 2 * 104
+moves only contains the characters 'U', 'D', 'L' and 'R'.
+ */
