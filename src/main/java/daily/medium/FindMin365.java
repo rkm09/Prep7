330c63148fb7c93d1c2def42846b8330f310cb9b
rkm09 <raksha.mishra09@gmail.com>
2026-05-16 05:23:43 +0530

    find min

--- src/main/java/daily/medium/FindMin365.java
+++ src/main/java/daily/medium/FindMin365.java
@@ -10,7 +10,7 @@ public class FindMin365 {
     public int findMin(int[] nums) {
         int left = 0, right = nums.length - 1;
         while(left < right) {
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2; // int mid = (left + right) >>> 1;
             if(nums[mid] > nums[right])
                 left = mid + 1;
             else
@@ -29,7 +29,7 @@ Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in 
 Given the sorted rotated array nums of unique elements, return the minimum element of this array.
 You must write an algorithm that runs in O(log n) time.
 Example 1:
-Input: nums = [3,4,5,1,2]
+Input: nums = [3,4,5,1,2
 Output: 1
 Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 Example 2:
